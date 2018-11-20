aa2d651972cbd128da891047f41e6827d4e5bbbc
Murphy3200860 <3200860@sps-student.sk.ca>
2018-11-20 13:29:18 -0600

    pacman

---
+++ pac-man/sketch.js
@@ -0,0 +1,56 @@
+// Connect Four
+// Charlie Murphy
+// Nov. 13, 2018
+//
+//
+let rows;
+let cols;
+let grid;
+let cellSize;
+
+function preload() {
+  grid = loadStrings("assets/stage1.txt");
+}
+
+function setup() {
+  createCanvas(700, 700);
+  background(220);
+  rows = grid[0].length;
+  cols = grid[0].length;
+  cellSize = width / cols -1;
+  cleanUpTheGrid();
+}
+
+function draw() {
+  background(255);
+  displayGrid();
+}
+
+//allows the text file to be used for initial grid
+function cleanUpTheGrid() {
+  for (let i = 0; i < grid.length; i++) {
+    grid[i] = grid[i].split(""); //turns it into a 2D array
+  }
+}
+
+
+function displayGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      // barrier
+      if (grid[y][x]=== "0") {
+        stroke(0);
+        fill(0, 0, 255);
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+      // play space
+      else if (grid[y][x] === "1") {
+        stroke(255);
+        fill(0);
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+      }
+    }
+  }
+}
+
+//placing pieces, alternating red and yellow each time
